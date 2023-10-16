6ab0aaf477a5907c04f68af6841bf46c41fa54c0
Lucas Moura de Almeida <lucas.almeida@ape11.com.br>
2023-10-16 20:49:47 -0300

    Atualização do método dos quaternion

--- atividade_1/quaternion.m
+++ atividade_1/quaternion.m
@@ -1,56 +1,15 @@
-function [D_CM] = quaternion(si, sb, mi, mb)
+function [result] = quaternion(R)
+    diag = trace(R);
+    q0 = sqrt(1 + diag) / 2;
+    q1 = (R(3,2) - R(2,3)) / (4 * q0);
+    q2 = (R(1,3) - R(3,1)) / (4 * q0);
+    q3 = (R(2,1) - R(1,2)) / (4 * q0);
 
-    % Normalizar os vetores
-    si = si / norm(si);
-    sb = sb / norm(sb);
-    mi = mi / norm(mi);
-    mb = mb / norm(mb);
+    magnitude = sqrt(q0^2 + q1^2 + q2^2 + q3^2)
+    q0 /= magnitude
+    q1 /= magnitude
+    q2 /= magnitude
+    q3 /= magnitude
 
-    % Calcular o vetor de referência no sistema inercial
-    N_i = [1; 0; 0];
-
-    % Calcular o vetor de referência no sistema do corpo
-    N_b = si;
-
-    % Calcular o vetor de referência após a rotação
-    B_i = sb;
-
-    % Calcular o erro entre o vetor de referência estimado e o verdadeiro
-    erro_quaternion = norm(B_i - N_i);
-
-    disp('Resultado do algoritmo de Quaternions:');
-    disp('Erro entre B_i estimado e N_i verdadeiro:');
-    disp(erro_quaternion);
-
-    % Calcular o vetor de referência no sistema inercial
-    N_i = [1; 0; 0];
-
-    % Calcular o quaternion que rotaciona si para sb
-    v = cross(si, sb);
-    q_scalar = 1 + dot(si, sb);
-    q_vector = v / norm(v) * sqrt(q_scalar);
-
-    % Normalizar o quaternion
-    q_vector = q_vector / norm(q_vector);
-    q_scalar = q_scalar / norm(q_scalar);
-
-    % Converter o quaternion para a matriz de rotação DCM
-    D_CM = [
-        1 - 2 * (q_vector(2)^2 + q_vector(3)^2), 2 * (q_vector(1) * q_vector(2) - q_scalar * q_vector(3)), 2 * (q_vector(1) * q_vector(3) + q_scalar * q_vector(2));
-        2 * (q_vector(1) * q_vector(2) + q_scalar * q_vector(3)), 1 - 2 * (q_vector(1)^2 + q_vector(3)^2), 2 * (q_vector(2) * q_vector(3) - q_scalar * q_vector(1));
-        2 * (q_vector(1) * q_vector(3) - q_scalar * q_vector(2)), 2 * (q_vector(2) * q_vector(3) + q_scalar * q_vector(1)), 1 - 2 * (q_vector(1)^2 + q_vector(2)^2)
-    ];
-
-    % Converter o vetor do magnetômetro do corpo para inercial usando a matriz de rotação DCM
-    mi_estimado = D_CM * mb;
-
-    % Calcular o erro entre o vetor do magnetômetro estimado e o verdadeiro
-    erro_quaternion = norm(mi_estimado - mi);
-
-    disp('Resultado do algoritmo de Quaternions:');
-    disp('Matriz de rotação D_CM:');
-    disp(D_CM);
-    disp('Erro entre mi estimado e mi verdadeiro:');
-    disp(erro_quaternion);
-
+    %quat2eul
 end
