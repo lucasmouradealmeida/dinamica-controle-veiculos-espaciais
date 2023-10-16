5d96098dc508db1a467288b7ccb50cb970909560
Lucas Moura de Almeida <lucas.almeida@ape11.com.br>
2023-10-16 20:41:24 -0300

    Adição do cálculo de erro

--- atividade_1/triad.m
+++ atividade_1/triad.m
@@ -25,10 +25,9 @@ function [R] = triad(si, sb, mi, mb)
     psi = atan2(-R(1,3), R(3,3))
     theta = asin(-R(2,3))
     phi = atan2(-R(2,1), R(2,2))
-
     
-
-    % disp('Erro estimado:');
-    % disp(erro_triad);
+    erro_triad = mb - R*mi;
+    disp('Erro estimado:');
+    disp(erro_triad);
 
 end
