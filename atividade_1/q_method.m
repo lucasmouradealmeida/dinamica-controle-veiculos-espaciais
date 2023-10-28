1b70bb2c52757109cbc9cb840cb79d631e203408
Lucas Moura de Almeida <lucas.almeida@ape11.com.br>
2023-10-27 23:27:42 -0300

    Atualização do método q

--- atividade_1/q_method.m
+++ atividade_1/q_method.m
@@ -1,11 +1,11 @@
 function [V, D] = q_method(si, sb, mi, mb)
-    vm = 1;
-    ws = 1;
+    vm = 0.5;
+    ws = 0.5;
     B = ws*(sb*(si)') + vm*(mb*(mi)');
     Z = [B(2,3)-B(3,2); B(3,1)-B(1,3); B(1,2)-B(2,1)];
     sigma = trace(B);
     S = B + B';
-    K = [S-sigma*eye(3) Z; Z' sigma]
+    K = [S-sigma*eye(3) Z; (Z)' sigma]
     [V, D] = eig(K)
 
     q1 = V(1,4);
@@ -13,5 +13,5 @@ function [V, D] = q_method(si, sb, mi, mb)
     q3 = V(3,4);
     q4 = V(4,4);
 
-    result = quad2eul([q1, q2, q3, q4], 'ZXY')
+    result = quad2eul([q4, q1, q2, q3], 'ZXY')
 end
