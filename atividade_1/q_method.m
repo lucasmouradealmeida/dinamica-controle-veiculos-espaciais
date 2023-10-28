d04cf988265c6165b0c4401934976f031acee308
Lucas Moura de Almeida <lucas.almeida@ape11.com.br>
2023-10-27 22:12:16 -0300

    Ajustes no método q

--- atividade_1/q_method.m
+++ atividade_1/q_method.m
@@ -7,4 +7,11 @@ function [V, D] = q_method(si, sb, mi, mb)
     S = B + B';
     K = [S-sigma*eye(3) Z; Z' sigma]
     [V, D] = eig(K)
+
+    q1 = V(1,4);
+    q2 = V(2,4);
+    q3 = V(3,4);
+    q4 = V(4,4);
+
+    result = quad2eul([q1, q2, q3, q4], 'ZXY')
 end
